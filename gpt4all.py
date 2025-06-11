a4f1535b6371218572d851395a1e7c37573d33fa
User1 <user1@User1s-MacBook-Pro.local>
2025-06-10 22:14:13 -0500

    README file is created

--- gpt4all.py
+++
@@ -1,6 +0,0 @@
-from gpt4all import GPT4All
-print(GPT4All)
-exit()
-model = GPT4All("Meta-Llama-3-8B-Instruct.Q4_0.gguf") # downloads / loads a 4.66GB LLM
-with model.chat_session():
-    print(model.generate("How can I run LLMs efficiently on my laptop?", max_tokens=1024))
